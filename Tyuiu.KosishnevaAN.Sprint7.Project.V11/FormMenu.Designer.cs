7ac112f12bba3de1b3cb47bf65b0a3f194e6acc7
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-12-22 20:41:38 +0500

    Task7

--- Tyuiu.KosishnevaAN.Sprint7.Project.V11/FormMenu.Designer.cs
+++ Tyuiu.KosishnevaAN.Sprint7.Project.V11/FormMenu.Designer.cs
@@ -30,82 +30,41 @@ namespace Tyuiu.KosishnevaAN.Sprint7.Project.V11
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonADD_KAN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridViewRabotniki_KAN = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.openFileDialogBD = new System.Windows.Forms.OpenFileDialog();
+            this.buttonSOHR_KAN = new System.Windows.Forms.Button();
+            this.saveFileDialogBD = new System.Windows.Forms.SaveFileDialog();
+            this.groupBoxADD_KAN = new System.Windows.Forms.GroupBox();
+            this.textBoxFOI_KAN = new System.Windows.Forms.TextBox();
+            this.textBoxTel_KAN = new System.Windows.Forms.TextBox();
+            this.textBoxDolgn_KAN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRabotniki_KAN)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.groupBoxADD_KAN.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1212, 174);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 174);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(644, 604);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // panel3
-            // 
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(644, 174);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(568, 604);
-            this.panel3.TabIndex = 2;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(626, 536);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(346, 14);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(292, 26);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 44);
-            this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(294, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "поиск";
             // 
             // label2
             // 
@@ -113,9 +72,155 @@ namespace Tyuiu.KosishnevaAN.Sprint7.Project.V11
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(239, 39);
+            this.label2.Size = new System.Drawing.Size(233, 38);
             this.label2.TabIndex = 0;
             this.label2.Text = "главное меню";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.buttonSOHR_KAN);
+            this.panel2.Controls.Add(this.dataGridViewRabotniki_KAN);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(644, 604);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "показать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonADD_KAN
+            // 
+            this.buttonADD_KAN.Location = new System.Drawing.Point(175, 162);
+            this.buttonADD_KAN.Name = "buttonADD_KAN";
+            this.buttonADD_KAN.Size = new System.Drawing.Size(156, 59);
+            this.buttonADD_KAN.TabIndex = 2;
+            this.buttonADD_KAN.Text = "добавить";
+            this.buttonADD_KAN.UseVisualStyleBackColor = true;
+            this.buttonADD_KAN.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(856, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "поиск";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(908, 9);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridViewRabotniki_KAN
+            // 
+            this.dataGridViewRabotniki_KAN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRabotniki_KAN.Location = new System.Drawing.Point(12, 56);
+            this.dataGridViewRabotniki_KAN.Name = "dataGridViewRabotniki_KAN";
+            this.dataGridViewRabotniki_KAN.RowHeadersWidth = 51;
+            this.dataGridViewRabotniki_KAN.RowTemplate.Height = 24;
+            this.dataGridViewRabotniki_KAN.Size = new System.Drawing.Size(626, 536);
+            this.dataGridViewRabotniki_KAN.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.groupBoxADD_KAN);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(644, 174);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(568, 604);
+            this.panel3.TabIndex = 2;
+            // 
+            // openFileDialogBD
+            // 
+            this.openFileDialogBD.FileName = "openFileDialog1";
+            // 
+            // buttonSOHR_KAN
+            // 
+            this.buttonSOHR_KAN.Location = new System.Drawing.Point(102, 6);
+            this.buttonSOHR_KAN.Name = "buttonSOHR_KAN";
+            this.buttonSOHR_KAN.Size = new System.Drawing.Size(90, 44);
+            this.buttonSOHR_KAN.TabIndex = 5;
+            this.buttonSOHR_KAN.Text = "Сохранить изменения";
+            this.buttonSOHR_KAN.UseVisualStyleBackColor = true;
+            this.buttonSOHR_KAN.Click += new System.EventHandler(this.buttonPLUS_KAN_Click);
+            // 
+            // groupBoxADD_KAN
+            // 
+            this.groupBoxADD_KAN.Controls.Add(this.label5);
+            this.groupBoxADD_KAN.Controls.Add(this.label4);
+            this.groupBoxADD_KAN.Controls.Add(this.label3);
+            this.groupBoxADD_KAN.Controls.Add(this.buttonADD_KAN);
+            this.groupBoxADD_KAN.Controls.Add(this.textBoxDolgn_KAN);
+            this.groupBoxADD_KAN.Controls.Add(this.textBoxTel_KAN);
+            this.groupBoxADD_KAN.Controls.Add(this.textBoxFOI_KAN);
+            this.groupBoxADD_KAN.Location = new System.Drawing.Point(40, 6);
+            this.groupBoxADD_KAN.Name = "groupBoxADD_KAN";
+            this.groupBoxADD_KAN.Size = new System.Drawing.Size(478, 234);
+            this.groupBoxADD_KAN.TabIndex = 6;
+            this.groupBoxADD_KAN.TabStop = false;
+            this.groupBoxADD_KAN.Text = "новый сотрудник";
+            // 
+            // textBoxFOI_KAN
+            // 
+            this.textBoxFOI_KAN.Location = new System.Drawing.Point(37, 47);
+            this.textBoxFOI_KAN.Name = "textBoxFOI_KAN";
+            this.textBoxFOI_KAN.Size = new System.Drawing.Size(416, 22);
+            this.textBoxFOI_KAN.TabIndex = 0;
+            // 
+            // textBoxTel_KAN
+            // 
+            this.textBoxTel_KAN.Location = new System.Drawing.Point(158, 134);
+            this.textBoxTel_KAN.Name = "textBoxTel_KAN";
+            this.textBoxTel_KAN.Size = new System.Drawing.Size(187, 22);
+            this.textBoxTel_KAN.TabIndex = 1;
+            // 
+            // textBoxDolgn_KAN
+            // 
+            this.textBoxDolgn_KAN.Location = new System.Drawing.Point(37, 92);
+            this.textBoxDolgn_KAN.Name = "textBoxDolgn_KAN";
+            this.textBoxDolgn_KAN.Size = new System.Drawing.Size(416, 22);
+            this.textBoxDolgn_KAN.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ФИО";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "должность";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "номер телефона";
             // 
             // FormMenu
             // 
@@ -131,8 +236,10 @@ namespace Tyuiu.KosishnevaAN.Sprint7.Project.V11
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRabotniki_KAN)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.groupBoxADD_KAN.ResumeLayout(false);
+            this.groupBoxADD_KAN.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -144,8 +251,19 @@ namespace Tyuiu.KosishnevaAN.Sprint7.Project.V11
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonADD_KAN;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewRabotniki_KAN;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialogBD;
+        private System.Windows.Forms.Button buttonSOHR_KAN;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogBD;
+        private System.Windows.Forms.GroupBox groupBoxADD_KAN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDolgn_KAN;
+        private System.Windows.Forms.TextBox textBoxTel_KAN;
+        private System.Windows.Forms.TextBox textBoxFOI_KAN;
     }
 }
--- Tyuiu.KosishnevaAN.Sprint7.Project.V11/FormMenu.cs
+++ Tyuiu.KosishnevaAN.Sprint7.Project.V11/FormMenu.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Tyuiu.KosishnevaAN.Sprint7.Project.V11.Lib;
 
 namespace Tyuiu.KosishnevaAN.Sprint7.Project.V11
 {
@@ -26,5 +28,60 @@ namespace Tyuiu.KosishnevaAN.Sprint7.Project.V11
         {
             //
         }
+        DataService ds = new DataService();
+        
+        private void button2_Click(object sender, EventArgs e) //ОТКРЫТЬ базу данных
+        {
+            dataGridViewRabotniki_KAN.ColumnCount = 8;
+
+            
+            using (var reader = new StreamReader("encoded-база данных (1).csv"))                 
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    dataGridViewRabotniki_KAN.Rows.Add(values);
+                }
+            }
+            
+
+        }
+
+        private void buttonPLUS_KAN_Click(object sender, EventArgs e)  //СОХРАНЕНИЕ
+        {
+            try
+            {
+                //Сохраняем изменения обратно в CSV файл
+                using (var writer = new StreamWriter("encoded-база данных (1).csv"))
+                {
+                    foreach (DataGridViewRow row in dataGridViewRabotniki_KAN.Rows)
+                    {
+                        writer.WriteLine(string.Join(";", Array.ConvertAll(row.Cells.Cast<DataGridViewCell>().ToArray(), cell => cell.Value.ToString())));
+                    }
+                }
+                MessageBox.Show("Данные успешно сохранены!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении данных: {ex.Message}");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) // ДОБАВЛЕНИЕ
+        {
+            string name = textBoxFOI_KAN.Text;
+            string position = textBoxDolgn_KAN.Text;
+            string specialization = textBoxTel_KAN.Text;
+            // заполнение
+            dataGridViewRabotniki_KAN.Rows.Add(name, position, specialization);
+            textBoxFOI_KAN.Text = "";
+            textBoxDolgn_KAN.Text = "";
+            textBoxTel_KAN.Text = "";
+            dataGridViewRabotniki_KAN.CurrentCell = dataGridViewRabotniki_KAN.Rows[dataGridViewRabotniki_KAN.Rows.Count - 1].Cells[0];
+        }
     }
+    
 }
